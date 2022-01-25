046503c9478d847421a8a341979792099b499a62
deepak-rudrainnovative <deepak.kumar@rudrainnovative.in>
2022-01-25 10:44:06 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express=require('express')
+
+const app=express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+const PORT=process.env.PORT||4000;
+
+app.get("/", (req, res) => {
+    res.status(200).json("Hello world");
+  });
+  
+module.exports = app.listen(PORT, () =>
+    console.log(`Running on port ${PORT}`)
+  );
