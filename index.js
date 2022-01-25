25bd196e2187ef4598ccaec5a2dc4f1fd07b51ef
deepak-rudrainnovative <deepak.kumar@rudrainnovative.in>
2022-01-25 12:19:47 +0530

    new route about added

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ const PORT=process.env.PORT||4000;
 app.get("/", (req, res) => {
     res.status(200).json("Hello world new");
   });
+app.get("/about", (req, res) => {
+    res.status(200).json("<h3>About Page</h3>");
+  });
   
 module.exports = app.listen(PORT, () =>
     console.log(`Running on port ${PORT}`)
