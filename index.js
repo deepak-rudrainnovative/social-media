3b9f57264880cb057a2441457764a02fe4e346dc
deepak-rudrainnovative <deepak.kumar@rudrainnovative.in>
2022-01-25 11:17:32 +0530

    new messages added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({extended:false}))
 const PORT=process.env.PORT||4000;
 
 app.get("/", (req, res) => {
-    res.status(200).json("Hello world");
+    res.status(200).json("Hello world new");
   });
   
 module.exports = app.listen(PORT, () =>
